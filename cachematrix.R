bc46f50e8d429905d8d430ab89843f87fe123347
Oriana Mosca <orianamosca@hotmail.it>
2014-12-22 01:22:58 +0100

    first commit

--- cachematrix.R
+++ cachematrix.R
@@ -5,16 +5,19 @@
 
 makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
-    m <- NULL
-    set <- function(y) {
+makeCacheMatrix <- function(x = matrix()) {      #  input x will be a matrix
+    m <- NULL                                    #  m will be our 'mean' and it's reset to 
+    set <- function(y) {                         #  time makeCacheMatrix is called                        
+                   
       x <<- y
       m <<- NULL
     }
-    get <- function() x
-    setmean <- function(mean) m <<- mean
-    getmean <- function() m
-    list(set = set, get = get,
+    get <- function() x                          # this function returns the value of the original matrix
+    setmean <- function(mean) m <<- mean         # this is called by cachemean() during the first cachemean()
+                                                          #  access and it will store the value using superassignment
+    getmean <- function() m                      # this will return the cached value to cachemean() on
+                                                 # subsequent accessess
+    list(set = set, get = get,                   # this is a list of the internal functions
          setmean = setmean,
          getmean = getmean)
   }
@@ -28,15 +31,15 @@ cacheSolve: This function computes the inverse of the special "matrix" returned 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+                                # the input x is an object created by makeCacheMatrix
+  m <- x$getmean()              # accesses the object 'x' and gets the value of the mean
+  if(!is.null(m)) {             # if mean was already cached (not NULL) ...
+    message("getting cached data") # ... send this message to the console
+    return(m)                       # ... and return the mean ... "return" ends 
+  }                                 #   the function cachemean(), note
+  data <- x$get()                   # we reach this code only if x$getmean() returned NULL
+  m <- mean(data, ...)              # if m was NULL then we have to calculate the mean
+  x$setmean(m)                      # store the calculated mean value in x (see setmean() in makeCacheMatrix
+  m                                 # return the mean to the code that called this function
 }
-cacheSolve <- function(x, ...) {
-  m <- x$getmean()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  data <- x$get()
-  m <- mean(data, ...)
-  x$setmean(m)
-  m
 }
